1234dca246b3336e361471b182c8cefa271247a9
camilo-mancipe <camilomancipe@outlook.com>
2022-09-10 11:15:30 -0500

    se corrigen rutas
    * se deshabilito la ruta register de auth
    * se modificó la ruta /

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
